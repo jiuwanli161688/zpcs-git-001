b7be855ba74d508fd53a122ddf7d91963e11d6b1
caopengwei <18811522476@163.com>
2025-11-30 17:42:13 +0800

    Update detail.js

--- api/shop/list/detail.js
+++ api/shop/list/detail.js
@@ -169,3 +169,72 @@ export function getCheckInDayCount({ storeId }) {
     params: { storeId },
   });
 }
+// -----
+
+/**
+ * 获取审核过的相册(相册列表)
+ * @param 
+ * @returns {
+  data: {
+    "success": true,
+    "message": "success",
+    "code": 200,
+    "timestamp": 1764466927866,
+    "result": {
+        "records": [
+            {
+                "id": "1994396572233801728",
+                "createBy": "admin",
+                "createTime": "2025-11-28 21:22:51",
+                "updateBy": "admin",
+                "updateTime": "2025-11-28 21:23:22",
+                "storeId": "1798246165884223489",
+                "imageId": null,
+                "imageTitle": "5d86baf3af894f2f9590554345ce466a.png",
+                "imageDesc": null,
+                "imageUrl": "https://zpwdsc.oss-cn-beijing.aliyuncs.com/MANAGER/cbd/album/6d19acf349e245948d8c171ae6a22535.png",
+                "fileKey": "MANAGER/cbd/album/6d19acf349e245948d8c171ae6a22535.png",
+                "images": null,
+                "storeName": "中品维度商城",
+                "storeNumber": null,
+                "auditStatus": 2,
+                "auditTime": "2025-11-28 21:23:22",
+                "qcUserName": "admin"
+            },
+            {
+                "id": "1004",
+                "createBy": "admin",
+                "createTime": "2025-11-27 10:15:00",
+                "updateBy": "admin",
+                "updateTime": "2025-11-28 21:33:44",
+                "storeId": "1798246165884223489",
+                "imageId": 3004,
+                "imageTitle": "门头招牌夜景",
+                "imageDesc": "照片不清晰，审核未通过",
+                "imageUrl": "https://img.example.com/store/2001/sign_front.jpg",
+                "fileKey": null,
+                "images": null,
+                "storeName": "中品维度商城",
+                "storeNumber": null,
+                "auditStatus": 2,
+                "auditTime": "2025-11-28 21:33:44",
+                "qcUserName": "admin"
+            }
+        ],
+        "total": 2,
+        "size": 10,
+        "current": 1,
+        "pages": 1
+    }
+}
+}
+ 
+ */
+export function getCheckInDayCount({ storeId, pageNumber, pageSize }) {
+  return http.request({
+    url: "/cbd/storeAlbumManage/approved/images",
+    method: Method.GET,
+    needToken: true,
+    params: { storeId, pageNumber, pageSize },
+  });
+}
